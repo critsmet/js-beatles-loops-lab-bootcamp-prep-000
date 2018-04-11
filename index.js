de03696c914773075bf7227d358887e23d0ab3bd
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-11 03:17:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var whoPlaysWhat = []; 
+  for (var i = 0; i < musicians.length; i++) {
+    whoPlaysWhat.push(musicians[i] + " plays " + instruments[i])
+  }
+  return whoPlaysWhat
+}
+function johnLennonFacts(facts) {
+  var johnLennonFactsAgain = []; 
+  var i = 0
+  while (i < facts.length) {
+    johnLennonFactsAgain.push(facts[i] + "!!!");
+    i++
+    }
+    return johnLennonFactsAgain
+  }
